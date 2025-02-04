d46338443cb14fc424c6a9c02b1a0958bb864f40
congflo <chucong491@gmail.com>
2025-02-04 13:37:36 +0700

    fix some code

--- JavaApp/src/lwjgl/org/lwjgl/glfw/GLFW.java
+++ JavaApp/src/lwjgl/org/lwjgl/glfw/GLFW.java
@@ -514,7 +514,7 @@ public class GLFW
 
     static {
         try {
-            System.load(System.getenv("BUNDLE_PATH") + "/PojavLauncher");
+            System.load(System.getenv("BUNDLE_PATH") + "/AngleLauncher");
         } catch (UnsatisfiedLinkError e) {
             e.printStackTrace();
         }
@@ -584,7 +584,7 @@ public class GLFW
         throw new UnsupportedOperationException();
     }
 
-    private static final SharedLibrary GLFW = new MacOSXLibraryDL("PojavLauncher", DynamicLinkLoader.RTLD_DEFAULT);
+    private static final SharedLibrary GLFW = new MacOSXLibraryDL("AngleLauncher", DynamicLinkLoader.RTLD_DEFAULT);
 
 
     /** Contains the function pointers loaded from the glfw {@link SharedLibrary}. */
