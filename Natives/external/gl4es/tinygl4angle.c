645b572718f26b942b60ae9278aac74d583a2d82
congflo <chucong491@gmail.com>
2025-02-04 17:27:39 +0700

    update for renderer

--- Natives/external/gl4es/tinygl4angle.c
+++ Natives/external/gl4es/tinygl4angle.c
@@ -4,9 +4,10 @@
 
 #define GL_GLEXT_PROTOTYPES
 
-#include "GL/gl.h"
-#include "GL/glext.h"
-#include "GLES3/gl32.h"
+#include "angle_gl.h"
+//#include "GL/gl.h"
+//#include "GL/glext.h"
+//#include "GLES3/gl32.h"
 #include "string_utils.h"
 
 #define LOOKUP_FUNC(func) \
